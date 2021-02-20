987c09f1cf912d780a2c150550505689eb322e94
nzelot <leontsteiner@gmail.com>
2021-02-20 15:43:07 +0100

    added option page functionality

--- include/kitchensound/pages/options_page.h
+++ include/kitchensound/pages/options_page.h
@@ -1,6 +1,8 @@
 #ifndef KITCHENSOUND_OPTIONS_PAGE_H
 #define KITCHENSOUND_OPTIONS_PAGE_H
 
+#include <vector>
+
 #include "kitchensound/pages/base_page.h"
 
 class OsUtil;
@@ -13,11 +15,18 @@ public:
     void* leave_page(PAGES destination) override;
     void handle_enter_key(InputEvent&) override;
     void handle_wheel_input(int delta) override;
+    void update_time() override;
 
     void render(Renderer& renderer) override;
 
 private:
     std::shared_ptr<OsUtil> _os;
+    struct {
+        int selection_idx;
+        int selection_offset;
+        int selection_limit;
+        std::vector<std::string> data;
+    } _model;
 };
 
 #endif //KITCHENSOUND_OPTIONS_PAGE_H
--- src/pages/options_page.cpp
+++ src/pages/options_page.cpp
@@ -6,13 +6,23 @@
 #include "kitchensound/renderer.h"
 #include <kitchensound/os_util.h>
 
+#define PAGE_SIZE 7
+
 OptionsPage::OptionsPage(StateController &ctrl, std::shared_ptr<OsUtil>& os)
-        : BasePage(OPTIONS, ctrl), _os{os} {}
+        : BasePage(OPTIONS, ctrl), _os{os}, _model{0, 0, 0, {}} {
+    _model.data.emplace_back("IPv4:\t" + _os->get_local_ip_address());
+    _model.data.emplace_back("Sys.up.:\t" + _os->get_system_uptime());
+    _model.data.emplace_back("Prg.up.:\t" + _os->get_program_uptime());
+    _model.data.emplace_back("Shutdown");
+    _model.data.emplace_back("Reboot");
+    _model.selection_limit = _model.data.size();
+}
 
 OptionsPage::~OptionsPage() = default;
 
 void OptionsPage::enter_page(PAGES origin, void *payload) {
     update_time();
+    _model.selection_idx = 0;
     spdlog::info("OptionsPage::enter_page(): from origin {0}", origin);
 }
 
@@ -21,20 +31,37 @@ void *OptionsPage::leave_page(PAGES destination) {
     return nullptr;
 }
 
+void OptionsPage::update_time() {
+    BasePage::update_time();
+    _model.data[0] = "Ipv4:\t" + _os->get_local_ip_address();
+    _model.data[1] = "Sys.up.:\t" + _os->get_system_uptime();
+    _model.data[2] = "Prg.up.:\t" + _os->get_program_uptime();
+}
+
 void OptionsPage::handle_enter_key(InputEvent& inev) {
-    if(inev.value == INEV_KEY_DOWN)
-        _os->trigger_reboot();
+    if(inev.value == INEV_KEY_DOWN){
+        if(_model.selection_idx == 3)
+            _os->trigger_shutdown();
+        else if(_model.selection_idx == 4)
+            _os->trigger_reboot();
+    }
 }
 
 void OptionsPage::handle_wheel_input(int delta) {
-    //NOP
+    _model.selection_idx += delta;
+    if(_model.selection_idx < 0)
+        _model.selection_idx = 0;
+    if(_model.selection_idx >= _model.selection_limit)
+        _model.selection_idx = _model.selection_limit -1;
 }
 
 void OptionsPage::render(Renderer& renderer) {
     this->render_time(renderer);
-    renderer.render_text(10, 40, "IPv4:\t" + _os->get_local_ip_address(), Renderer::LARGE, Renderer::LEFT);
-    renderer.render_text(10, 70,  "Sys. Up.: " + _os->get_system_uptime(), Renderer::LARGE, Renderer::LEFT);
-    renderer.render_text(10, 100, "Prg. Up.: " + _os->get_program_uptime(), Renderer::LARGE, Renderer::LEFT);
-    renderer.render_text(10, 130, "Shutdown", Renderer::LARGE, Renderer::LEFT);
-    renderer.render_text(10, 160, "Reboot", Renderer::LARGE, Renderer::LEFT);
+    for (int i = _model.selection_offset; i < _model.selection_limit && i < (_model.selection_offset + PAGE_SIZE); ++i) {
+        if(i == _model.selection_idx){
+            renderer.render_rect(0, 32 + i*30, 320, 30, Renderer::HIGHLIGHT);
+        }
+
+        renderer.render_text(10, 46 + i*30, _model.data[i], Renderer::LARGE, Renderer::LEFT);
+    }
 }
